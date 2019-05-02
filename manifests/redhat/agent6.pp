2794530eb7ec25abe82a99b03b82ea63dbaec719
Fabian Dammekens <fabian.dammekens@cegeka.be>
2019-05-02 14:42:08 +0200

    [RENTA] Datadog agent

--- manifests/redhat/agent6.pp
+++ manifests/redhat/agent6.pp
@@ -16,28 +16,12 @@ class datadog_agent::redhat::agent6(
 
     validate_string($baseurl)
 
-    remote_file { 'DATADOG_RPM_KEY.public':
-        owner  => root,
-        group  => root,
-        mode   => '600',
-        path   => $public_key_local,
-        source => $gpgkey
-    }
-
-    exec { 'install-gpg-key':
-        command => "/bin/rpm --import ${public_key_local}",
-        onlyif  => "/usr/bin/gpg --quiet --with-fingerprint -n ${public_key_local} | grep \'A4C0 B90D 7443 CF6E 4E8A  A341 F106 8E14 E094 22B3\'",
-        unless  => '/bin/rpm -q gpg-pubkey-e09422b3',
-        require => Remote_file['DATADOG_RPM_KEY.public'],
-    }
-
     yumrepo {'datadog':
       enabled  => 1,
       gpgcheck => 1,
-      gpgkey   => 'https://yum.datadoghq.com/DATADOG_RPM_KEY.public',
+      gpgkey   => 'https://yum.datadoghq.com/DATADOG_RPM_KEY.public https://yum.datadoghq.com/DATADOG_RPM_KEY_E09422B3.public',
       descr    => 'Datadog, Inc.',
-      baseurl  => $baseurl,
-      require  => Exec['install-gpg-key'],
+      baseurl  => $baseurl
     }
 
     Package { require => Yumrepo['datadog']}
