3a9366eb688e73c0b3a68690d4322c8c5e66eb9c
NickIndeherberge <nick.indeherberge@cegeka.com>
2020-04-03 15:52:16 +0200

    [Datadog] Update profile, node definitions, redo manual changes

--- manifests/redhat.pp
+++ manifests/redhat.pp
@@ -15,11 +15,11 @@ class datadog_agent::redhat(
     case $agent_major_version {
       5 : {
         $defaulturl = "https://yum.datadoghq.com/rpm/${::architecture}/"
-        $gpgkey = 'https://yum.datadoghq.com/DATADOG_RPM_KEY.public'
+        $gpgkey = 'https://yum.datadoghq.com/DATADOG_RPM_KEY.public https://yum.datadoghq.com/DATADOG_RPM_KEY_E09422B3.public'
       }
       6 : {
         $defaulturl = "https://yum.datadoghq.com/stable/6/${::architecture}/"
-        $gpgkey = 'https://yum.datadoghq.com/DATADOG_RPM_KEY.public'
+        $gpgkey = 'https://yum.datadoghq.com/DATADOG_RPM_KEY.public https://yum.datadoghq.com/DATADOG_RPM_KEY_E09422B3.public'
       }
       7 : {
         $defaulturl = "https://yum.datadoghq.com/stable/7/${::architecture}/"
@@ -32,23 +32,6 @@ class datadog_agent::redhat(
       $baseurl = $agent_repo_uri
     } else {
       $baseurl = $defaulturl
-    }
-
-    $public_key_local = '/etc/pki/rpm-gpg/DATADOG_RPM_KEY.public'
-
-    file { 'DATADOG_RPM_KEY_E09422B3.public':
-        owner  => root,
-        group  => root,
-        mode   => '0600',
-        path   => $public_key_local,
-        source => 'https://yum.datadoghq.com/DATADOG_RPM_KEY_E09422B3.public'
-    }
-
-    exec { 'install-gpg-key':
-        command => "/bin/rpm --import ${public_key_local}",
-        onlyif  => "/usr/bin/gpg --dry-run --quiet --with-fingerprint -n ${public_key_local} | grep 'A4C0 B90D 7443 CF6E 4E8A  A341 F106 8E14 E094 22B3' || gpg --dry-run --import --import-options import-show ${public_key_local} | grep 'A4C0B90D7443CF6E4E8AA341F1068E14E09422B3'",
-        unless  => '/bin/rpm -q gpg-pubkey-e09422b3',
-        require => File['DATADOG_RPM_KEY_E09422B3.public'],
     }
 
     yumrepo { 'datadog-beta':
@@ -69,7 +52,6 @@ class datadog_agent::redhat(
       gpgkey   => $gpgkey,
       descr    => 'Datadog, Inc.',
       baseurl  => $baseurl,
-      require  => Exec['install-gpg-key'],
     }
 
     Package { require => Yumrepo['datadog6']}
